a2ab923a1cf363e6cf86530a412ad0e25254590b
Anna Virchenko <anna.virchenko.work@gmail.com>
2025-11-12 21:00:38 -0500

    feat(sep16): add ClickNextButtonStep2 feature and Playwright step implementation

--- features/payment-plans/ClickNextButtonStep2.feature
+++ features/payment-plans/ClickNextButtonStep2.feature
@@ -1,44 +1,49 @@
 @sep16
-Feature: Click on the next button on payment plans page   #! Test Only
+Feature: Click on the Next button on the Payment Plans page
 
-    As a customer, I should be able to click on the next button on step 2 when I select a plan.
+    As a customer, I should be able to click the Next button on Step 2 after selecting a plan.
 
-    #* AC1: Clicking on any plan should activate the next button
-    #* AC2: When the customer clicks on the next button, the Step 3 page should be displayed.
-    #* AC3: In the stepper, steps 1 and 2 should be green, and step 3 should be blue.
-    #* AC4: The payment component should be displayed.
-    #* AC5: A price summary should be displayed.
-    #* AC6: The back button should be displayed.
-    #* AC7: By default, the pay button should be displayed.
+    # AC1: Clicking any plan should activate the Next button.
+    # AC2: Clicking Next should display Step 3.
+    # AC3: In the stepper, Steps 1 & 2 are green, Step 3 is blue on Step 3.
+    # AC4: The payment component should be displayed.
+    # AC5: A price summary should be displayed.
+    # AC6: The Back button should be displayed.
 
+    # Colors used by the stepper:
+    #   Green: rgb(172, 245, 138)
+    #   Blue : rgb(1, 201, 255)
 
     Background:
-        Given user is on the enrollment page
-        And user has completed step one with valid information
-        And user is on step two of the enrollment process
+        Given User is on the enrollment page
+        And User completed the start application step
 
+    @sep16-1
+    Scenario: Next button activates after selecting a plan
+        Then The next button on step two should be disabled by default
+        When User selects upfront payment plan
+        Then The next button on step two should be enabled
 
-    #TODO: Create scenarios that cover all the acceptance criteria
+    @sep16-2
+    Scenario: Stepper colors update when proceeding to Step 3
+        Then Step one stepper should be green
+        And Step two stepper should be blue
+        When User selects upfront payment plan
+        And User clicks the next button on payment plan page
+        Then Step one stepper should be green
+        And Step two stepper should be green
+        And Step three stepper should be blue
 
-   Scenario: verify that the next button is disabled by default
-        Given user is on the enrollment page
-        And user has completed step one with valid information
-        And user is on step two of the enrollment process
-        Then the next button is disabled by default
+    @sep16-3
+    Scenario: Price summary is shown for each plan selection
+        When User selects upfront payment plan
+        Then The upfront payment summary should be displayed
+        When User selects installments payment plan
+        Then The installment payment summary should be displayed
 
-
-    Scenario: verify that the next button will be activated when user selects upfront payment option
-        Given user is on the enrollment page
-        And user has completed step one with valid information
-        And user is on step two of the enrollment process
-        When user clicks upfront payment option
-        Then the next button will be enabled
-
-    Scenario: verify that the next button will be activated when user selects installments payment option
-        Given user is on the enrollment page
-        And user has completed step one with valid information
-        And user is on step two of the enrollment process
-        When user clicks installments  payment option
-        Then the next button will be enabled
-
-
+    @sep16-4
+    Scenario: Back button is present and navigates back to Step 1
+        Then The back button is displayed on the payment plan page
+        And The back button is enabled on the payment plan page
+        When User clicks the back button on payment plan page
+        Then Step one stepper should be blue
--- steps/payment-plans/clickNextButtonStep2.ts
+++ steps/payment-plans/clickNextButtonStep2.ts
@@ -1,23 +1,141 @@
-import { Given, Then, When } from "@cucumber/cucumber";
-import { expect} from "@playwright/test";
-import { paymentPlanPage, page } from "../../globalPagesSetup";
-import { productInfo } from "../../utilities/qa-data-reader";
+import { Then, When } from '@cucumber/cucumber';
+import { expect } from '@playwright/test';
+import { paymentPlanPage, startApplicationPage } from '../../globalPagesSetup';
+import { microSettle } from '../../utilities/Browserutility';
+import { CustomWorld } from '../../hooks/globalHooks';
 
+/* ========== AC1: Next button default state ========== */
+Then(
+  'The next button on step two should be disabled by default',
+  async function (this: CustomWorld): Promise<void> {
+    await expect(paymentPlanPage.inactiveNextButton).toBeVisible();
+    await expect(paymentPlanPage.inactiveNextButton).toBeDisabled();
+  }
+);
 
-Given('user has completed step one with valid information', async function () {
-});
+/* ========== AC2: Enabling Next after selecting plan ========== */
+When(
+  'User selects upfront payment plan',
+  async function (this: CustomWorld): Promise<void> {
+    await paymentPlanPage.selectPaymentPlan('upfront');
+    await microSettle(this.page); // wait for expand/DOM updates
+  }
+);
 
-Given('user is on step two of the enrollment process', async function () {
-});
+Then(
+  'The next button on step two should be enabled',
+  async function (this: CustomWorld): Promise<void> {
+    await expect(paymentPlanPage.activeNextButton).toBeVisible();
+    await expect(paymentPlanPage.activeNextButton).toBeEnabled();
+  }
+);
 
-Then('the next button is disabled by default', async function () {
-});
+/* ========== AC3: Stepper colors ========== */
+Then(
+  'Step one stepper should be green',
+  async function (this: CustomWorld): Promise<void> {
+    await expect(startApplicationPage.startApplicationStepCircle).toHaveCSS(
+      'background-color',
+      'rgb(172, 245, 138)'
+    );
+  }
+);
 
-When('user clicks upfront payment option', async function () {
-});
+Then(
+  'Step two stepper should be blue',
+  async function (this: CustomWorld): Promise<void> {
+    await expect(startApplicationPage.paymentPlanStepCircle).toHaveCSS(
+      'background-color',
+      'rgb(1, 201, 255)'
+    );
+  }
+);
 
-Then('the next button will be enabled', async function () {
-});
+When(
+  'User clicks the next button on payment plan page',
+  async function (this: CustomWorld): Promise<void> {
+    await paymentPlanPage.clickNextButton();
+    await microSettle(this.page);
+  }
+);
 
-When('user clicks installments  payment option', async function () {
-});
+Then(
+  'Step two stepper should be green',
+  async function (this: CustomWorld): Promise<void> {
+    await expect(startApplicationPage.paymentPlanStepCircle).toHaveCSS(
+      'background-color',
+      'rgb(172, 245, 138)'
+    );
+  }
+);
+
+Then(
+  'Step three stepper should be blue',
+  async function (this: CustomWorld): Promise<void> {
+    await expect(startApplicationPage.reviewStepCircle).toHaveCSS(
+      'background-color',
+      'rgb(1, 201, 255)'
+    );
+  }
+);
+
+/* ========== AC5: Price summary appears for each plan ========== */
+Then(
+  'The upfront payment summary should be displayed',
+  async function (this: CustomWorld): Promise<void> {
+    await expect(paymentPlanPage.basePriceAmountUnderUpfront).toBeVisible();
+    await expect(paymentPlanPage.upfrontDiscountAmountUnderUpfront).toBeVisible();
+    await expect(paymentPlanPage.subtotalAmountUnderUpfront).toBeVisible();
+  }
+);
+
+When(
+  'User selects installments payment plan',
+  async function (this: CustomWorld): Promise<void> {
+    await paymentPlanPage.selectPaymentPlan('installments');
+    await microSettle(this.page);
+  }
+);
+
+Then(
+  'The installment payment summary should be displayed',
+  async function (this: CustomWorld): Promise<void> {
+    await expect(paymentPlanPage.basePriceAmountUnderInstallments).toBeVisible();
+    await expect(paymentPlanPage.installmentsNumberUnderInstallments).toBeVisible();
+    await expect(paymentPlanPage.pricePerInstallmentsAmountUnderInstallments).toBeVisible();
+    await expect(paymentPlanPage.firstMonthPaymentAmountUnderInstallments).toBeVisible();
+  }
+);
+
+/* ========== AC6: Back button presence + navigation ========== */
+Then(
+  'The back button is displayed on the payment plan page',
+  async function (this: CustomWorld): Promise<void> {
+    await expect(paymentPlanPage.backButton).toBeVisible();
+  }
+);
+
+Then(
+  'The back button is enabled on the payment plan page',
+  async function (this: CustomWorld): Promise<void> {
+    await expect(paymentPlanPage.backButton).toBeEnabled();
+  }
+);
+
+When(
+  'User clicks the back button on payment plan page',
+  async function (this: CustomWorld): Promise<void> {
+    await paymentPlanPage.backButton.click();
+    await microSettle(this.page);
+  }
+);
+
+Then(
+  'Step one stepper should be blue',
+  async function (this: CustomWorld): Promise<void> {
+    await expect(startApplicationPage.startApplicationStepCircle).toHaveCSS(
+      'background-color',
+      'rgb(1, 201, 255)'
+    );
+  }
+);
